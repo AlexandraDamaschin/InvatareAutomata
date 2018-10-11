54506e7f5090d2cdc2d2eefe61489fd01cdd02cb
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-10-11 16:44:53 +0300

    basic prj

---
+++ DrawDotsApp/DrawDotsApp/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawDotsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics graphics = this.CreateGraphics();
+            graphics.DrawEllipse(Pens.Black, 0, 300, 300, 50); 
+
+        }
+    }
+}
