54e2551842e70c0c4df9ef5165c70725617ee78f
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-10-11 17:18:52 +0300

    calculateNewCoordonate

--- DrawDotsApp/DrawDotsApp/Form1.cs
+++ DrawDotsApp/DrawDotsApp/Form1.cs
@@ -12,8 +12,14 @@ namespace DrawDotsApp
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //get current x and y 
+            int x = 0;
+            int y = 0;
+
             //set zones 
             Zone z1 = new Zone(120, 30, 10, 5, Pens.Blue);
+            calculateNewCoordonate(z1.getX());
+            calculateNewCoordonate(z1.getY());
 
             //draw points
             Graphics graphics = this.CreateGraphics();
@@ -21,6 +27,12 @@ namespace DrawDotsApp
 
 
             graphics.DrawEllipse(z1.getCuloare(), z1.getX(), z1.getY(), 5, 5);
+        }
+
+        private int calculateNewCoordonate(int coordonate)
+        {
+            int newCoordonate = coordonate + 300;
+            return newCoordonate;
         }
     }
 }
