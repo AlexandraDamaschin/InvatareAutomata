ca93ff7b2ce6f8adac540d0750baec2af2b594a7
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-10-11 16:58:51 +0300

    zone class

---
+++ DrawDotsApp/DrawDotsApp/Zone.cs
@@ -0,0 +1,42 @@
+﻿namespace DrawDotsApp
+{
+    public class Zone
+    {
+        private int mx;
+        private int my;
+        private int delta_x;
+        private int delta_y;
+        private int culoare;
+
+        //constructor
+        public Zone(int x, int y, int deltax, int deltay, int cul)
+        {
+            mx = x;
+            my = y;
+            deltax = delta_x;
+            deltay = delta_y;
+            culoare = cul;
+        }
+
+        public int getX()
+        {
+            return mx;
+        }
+        public int getY()
+        {
+            return my;
+        }
+        public int getDeltaX()
+        {
+            return delta_x;
+        }
+        public int getDeltaY()
+        {
+            return delta_y;
+        }
+        public int getCuloare()
+        {
+            return culoare;
+        }
+    }
+}
