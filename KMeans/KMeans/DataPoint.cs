e89d4e5ad3156845fb51658cc259971d3b275bfa
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-10-21 13:26:01 +0300

    DataPoint class

---
+++ KMeans/KMeans/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMeans
+{
+    class DataPoint
+    {
+        public int x;
+        public int y;
+        public int center;
+
+        public DataPoint(int x, int y, int center)
+        {
+            this.x = x;
+            this.y = y;
+            this.center = center;
+        }
+    }
+}
