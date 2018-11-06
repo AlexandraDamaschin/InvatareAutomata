1f58c02d8fd2a481d991c245d2cc49fee14b55cb
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-11-06 20:36:00 +0200

    distance

--- KMeans/KMeans/Form1.cs
+++ KMeans/KMeans/Form1.cs
@@ -201,6 +201,7 @@ namespace KMeans
         private double getDistance(int x1, int y1, int x2, int y2)
         {
             double distance;
+
             // Manhattan
             // distance = Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
 
