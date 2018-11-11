29c127d778b5cabdd4e80c3652edc70498cd604c
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-11-11 15:24:48 -0800

    DrawPoint

--- KohonenSOM/KohonenSOM/Form1.cs
+++ KohonenSOM/KohonenSOM/Form1.cs
@@ -21,11 +21,24 @@ namespace KohonenSOM
         public Neuroni[,] neuronis = new Neuroni[10, 10];
 
         #region Points
-        private void button_Points_Click(object sender, EventArgs e)
+        private void DrawPoint(int x, int y)
         {
-            DrawPoints();
-            InitializeNeuroni();
-            DrawMap();
+            Graphics graphics = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen pen = new Pen(Color.Black, 2);
+            graphics.DrawLine(pen, 300, 0, 300, 600);
+            graphics.DrawLine(pen, 0, 300, 600, 300);
+
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+            Point point = new Point(x, y);
+            Size size = new Size(1, 1);
+            Rectangle rectangle = new Rectangle(point, size);
+            graphics.FillRectangle(solidBrush, rectangle);
+            graphics.Dispose();
+        }
+
+        private void DrawPoints()
+        {
+            throw new NotImplementedException();
         }
 
         private void DrawMap()
@@ -38,9 +51,13 @@ namespace KohonenSOM
             throw new NotImplementedException();
         }
 
-        private void DrawPoints()
+
+
+        private void button_Points_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            DrawPoints();
+            InitializeNeuroni();
+            DrawMap();
         }
 
         #endregion
