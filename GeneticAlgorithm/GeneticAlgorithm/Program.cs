d2b295a7b4daa90af286060fa145de5dbd7a3697
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-03 17:30:59 +0200

    initializeFile

--- GeneticAlgorithm/GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/GeneticAlgorithm/Program.cs
@@ -1,7 +1,33 @@
-﻿namespace GeneticAlgorithm
+﻿using System;
+using System.IO;
+
+namespace GeneticAlgorithm
 {
     class Program
     {
+        StreamWriter streamWriter;
+        int maxNumber = 30;
+        int maxLenght = 64;
+        int generations = 0;
+        Chromosome[] chromosomes;
+        int ct = 0;
+        Random random = new Random();
+
+        void initializeFile()
+        {
+            try
+            {
+                streamWriter = new StreamWriter("Out.txt");
+            }
+            catch (Exception ex)
+            {
+                ct++;
+                initializeFile();
+                Console.WriteLine(ex);
+            }
+        }
+        
+
         static void Main(string[] args)
         {
         }
