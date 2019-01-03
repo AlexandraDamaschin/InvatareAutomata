bb170cb20f5ddb89ad228fa73ee02eb16367d3cc
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-03 17:34:32 +0200

    read steps and number of chromozomes

--- GeneticAlgorithm/GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/GeneticAlgorithm/Program.cs
@@ -6,7 +6,7 @@ namespace GeneticAlgorithm
     class Program
     {
         StreamWriter streamWriter;
-        int maxNumber = 30;
+        int maxNumberOfChromosomes = 30;
         int maxLenght = 64;
         int generations = 0;
         Chromosome[] chromosomes;
@@ -27,6 +27,16 @@ namespace GeneticAlgorithm
             }
         }
         
+        void initializeAlgorithm()
+        {
+            Console.WriteLine("Number of steps:");
+            generations = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number of chromosomes:");
+            maxNumberOfChromosomes = Convert.ToInt32(Console.ReadLine());
+
+            chromosomes = new Chromosome[maxNumberOfChromosomes];
+        }
 
         static void Main(string[] args)
         {
