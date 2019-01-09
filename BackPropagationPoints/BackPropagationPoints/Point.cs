eb05320e2aed6880f6c3f3ff845f4542904dd3ef
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-09 07:47:22 +0200

    point class

---
+++ BackPropagationPoints/BackPropagationPoints/Point.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace BackPropagationPoints
+{
+    class Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public int Area { get; set; }
+
+        public Color Color { get; set; }
+    }
+}
