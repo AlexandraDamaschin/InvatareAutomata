c2934cd99fb08fc9b696bd204148c891fcfa993c
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-09 07:53:53 +0200

    layers

---
+++ BackPropagationPoints/BackPropagationPoints/Layer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackPropagationPoints
+{
+    class Layer
+    {
+        public List<Neuron> Neurons { get; set; }
+
+        public Layer(int noNeurons)
+        {
+            Neurons = new List<Neuron>();
+
+            for (int i = 0; i < noNeurons; i++)
+            {
+                Neurons.Add(new Neuron());
+            }
+        }
+
+        public Layer(int noNeurons, int noWeights)
+        {
+            Neurons = new List<Neuron>();
+
+            for (int i = 0; i < noNeurons; i++)
+            {
+                Neurons.Add(new Neuron(noWeights));
+            }
+        }
+    }
+}
