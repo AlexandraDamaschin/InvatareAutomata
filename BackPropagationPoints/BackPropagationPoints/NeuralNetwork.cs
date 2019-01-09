ca199ce783205c1b20b96d74c72adfe2b204ef10
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-09 10:34:08 +0200

    unfinalized version

---
+++ BackPropagationPoints/BackPropagationPoints/NeuralNetwork.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BackPropagationPoints
+{
+    class NeuralNetwork
+    {
+        private double learningRate;
+
+        public List<Layer> Layers { get; set; }
+
+        public NeuralNetwork(int noInputs, int noHiddens, int noOutputs, double learningRate = 1.0)
+        {
+            this.learningRate = learningRate;
+
+            Layers = new List<Layer>();
+
+        }
+    }
+}
