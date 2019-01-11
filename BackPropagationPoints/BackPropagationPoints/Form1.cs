1974fd5e6203188577eb9fbd2faccbd804e42818
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-01-11 08:03:02 +0200

    colors initialized

--- BackPropagationPoints/BackPropagationPoints/Form1.cs
+++ BackPropagationPoints/BackPropagationPoints/Form1.cs
@@ -1,9 +1,51 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace BackPropagationPoints
 {
     public partial class Form1 : Form
     {
+        private const int maxX = 600;
+        private const int maxY = 600;
+
+        Graphics graphics;
+        bool done = false;
+        List<Point> points = new List<Point>();
+        int numberOfOutputs = 0;
+        NeuralNetwork neuralNetwork;
+
+        Color[] colorAreas = new Color[]
+        {
+            Color.FromArgb(0, 204, 255),
+            Color.FromArgb(204, 255, 153),
+            Color.FromArgb(255, 204, 153),
+            Color.FromArgb(223, 255, 128)
+        };
+
+        Color[] colorPoints = new Color[]
+        {
+             Color.FromArgb(0, 0, 179),
+            Color.FromArgb(0, 204, 102),
+            Color.FromArgb(204, 122, 0),
+            Color.FromArgb(134, 179, 0)
+
+        };
+
+        Color[] colors = new Color[]
+       {
+            Color.Red,
+            Color.Blue,
+            Color.Green,
+            Color.Firebrick,
+            Color.Orange,
+            Color.DarkViolet,
+            Color.Cyan,
+            Color.Turquoise,
+            Color.Yellow,
+            Color.Magenta
+       };
+
         public Form1()
         {
             InitializeComponent();
